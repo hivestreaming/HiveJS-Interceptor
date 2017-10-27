6f4557c45bdc95c227dfb88c69192b971f3cc188
Claudio Di Cosmo <claudio.dicosmo@hivestreaming.com>
2017-10-27 15:46:40 +0200

    HTB-952 vasic unit tests of XHRInterceptor

--- __tests__/hivejs.interceptor.spec.ts
+++ __tests__/hivejs.interceptor.spec.ts
@@ -1,11 +1,30 @@
-
+// tslint:disable-next-line:interface-name
+interface Window {
+  activateXHRInterceptor: any;
+  deactivateXHRInterceptor: any;
+  HiveOriginalXMLHttpRequest: any;
+}
+declare var window: Window;
+chai.should();
 
 describe('Generic Tests for HiveJS XHR Interceptor:', () => {
   describe('when no plugin is availalbe', () => {
-    it('opening a manifest or fragment URL will generate a normal XHR', () => {
-		XMLHttpRequest['name'].should.equal('HiveXMLHttpRequest')
-	  const testXhr = new XMLHttpRequest();
-	  testXhr.open('GET', 'http://test.fakedomain.com/fakemanifest.m3u8');
+    it('by default the interceptor is deactivated even when opening a manifest or fragment', () => {
+      XMLHttpRequest['name'].should.equal('XMLHttpRequest');
+      const testXhr: any = new XMLHttpRequest();
+      testXhr.open('GET', 'http://test.fakedomain.com/fakemanifest.m3u8');
+      testXhr.should.not.have.property('innerXhr');
+    });
+
+    it('can be activated and the fragments and manifest will be correctly intercepted', () => {
+      window.activateXHRInterceptor();
+      XMLHttpRequest['name'].should.equal('HiveXMLHttpRequest');
+      const testXhr: any = new XMLHttpRequest();
+      testXhr.open('GET', 'http://test.fakedomain.com/fakemanifest.m3u8');
+      testXhr.should.have.property('innerXhr');
+      testXhr.innerXhr.should.be.an.instanceof(
+        window.HiveOriginalXMLHttpRequest
+      );
     });
   });
 });
