ff8448009e1552c1e3f84b854e6faec520808885
Claudio Di Cosmo <claudio.dicosmo@hivestreaming.com>
2017-10-27 18:02:36 +0200

    HTB-952 checking object status evolution from xhr init to send with and without interceptor

--- __tests__/hivejs.interceptor.spec.ts
+++ __tests__/hivejs.interceptor.spec.ts
@@ -7,6 +7,37 @@ interface Window {
 declare var window: Window;
 chai.should();
 
+function xhrGenAndSend(done?) {
+  const testXhr: any = new XMLHttpRequest();
+  const statusesList = [];
+  testXhr.onreadystatechange = event => {
+    console.log('event', event);
+
+    statusesList.push({
+      readyState: testXhr.readyState,
+      response: testXhr.response,
+      responseText: testXhr.responseText,
+      responseType: testXhr.responseType,
+      responseURL: testXhr.responseURL,
+      responseXML: testXhr.responseXML,
+      status: testXhr.status,
+      statusText: testXhr.statusText,
+      timeout: testXhr.timeout,
+      upload: testXhr.upload,
+      withCredentials: testXhr.withCredentials,
+    });
+
+    if (event.currentTarget.readyState === 4) {
+      console.log('STATUSES: ', statusesList);
+      if (done) done();
+    }
+  };
+
+  testXhr.open('GET', 'http://ams-live.hivestreaming.com/manifest.m3u8');
+
+  testXhr.send();
+}
+
 describe('Generic Tests for HiveJS XHR Interceptor:', () => {
   describe('when no plugin is availalbe', () => {
     it('by default the interceptor is deactivated even when opening a manifest or fragment', () => {
@@ -16,7 +47,7 @@ describe('Generic Tests for HiveJS XHR Interceptor:', () => {
       testXhr.should.not.have.property('innerXhr');
     });
 
-    it('can be activated and the fragments and manifest will be correctly intercepted', () => {
+    it('can be activated and the fragments and manifest will be handled with the usual XMLHttpRequest', () => {
       window.activateXHRInterceptor();
       XMLHttpRequest['name'].should.equal('HiveXMLHttpRequest');
       const testXhr: any = new XMLHttpRequest();
@@ -25,6 +56,27 @@ describe('Generic Tests for HiveJS XHR Interceptor:', () => {
       testXhr.innerXhr.should.be.an.instanceof(
         window.HiveOriginalXMLHttpRequest
       );
+      window.deactivateXHRInterceptor();
+    });
+
+    it('can be activated and deactivated and the basic XMLHttpRequest is restored', () => {
+      XMLHttpRequest['name'].should.equal('XMLHttpRequest');
+
+      window.activateXHRInterceptor();
+      XMLHttpRequest['name'].should.equal('HiveXMLHttpRequest');
+
+      window.deactivateXHRInterceptor();
+      XMLHttpRequest['name'].should.equal('XMLHttpRequest');
+    });
+
+    it('has the same statuses as a normal XHR when activated and going thorugh the all open-send workflow', done => {
+      window.activateXHRInterceptor();
+
+      xhrGenAndSend();
+
+      window.deactivateXHRInterceptor();
+
+      xhrGenAndSend(done);
     });
   });
 });
