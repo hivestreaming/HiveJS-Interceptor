1607362d5f0fa2049bf4c4acc12a3389d1f7c86d
Omar Attia <omar.attia@hivestreaming.com>
2023-08-28 10:12:18 +0200

    restore old webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,44 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      }
+var webpack = require('webpack');
+
+module.exports = env => {
+
+  return {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+          title: 'our project', 
+          template: 'example/example-fetch.html' }),
+      new webpack.DefinePlugin({
+            'METADATA_EXTENTION': JSON.stringify(env.METADATA_EXTENTION),
+            'DATA_EXTENTION': JSON.stringify(env.DATA_EXTENTION),
+            'MORE_VERBOSE': env.MORE_VERBOSE
+        })
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'our project', 
-        template: 'example/example-fetch.html' }) 
-   ],
-
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    compress: true,
-    port: 4000,
-  },
+    devServer: {
+      static: path.join(__dirname, "dist"),
+      compress: true,
+      port: 4000,
+    },
+  }
 };
